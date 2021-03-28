79687951ebc6f6b1f915259173c0388409c9d6e0
chavalk <chavalk@hotmail.com>
2021-03-27 22:24:43 -0500

    Add server.js and code to connect to a Mongo DB.

---
+++ server.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+// Connect to the Mongo DB
+mongoose.connect(
+    process.env.MONGODB_URI || "mongodb://localhost/googlebooks",
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    }
+);
