a24ed4469bb3889ca396385575a30cc812a85d0c
chavalk <chavalk@hotmail.com>
2021-03-28 15:08:29 -0500

    Add jumbotron component and add component to application in app.js.

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Search from "./pages/Search";
 import Saved from "./pages/Saved";
 import Nav from "./components/Nav";
+import Jumbotron from "./components/Jumbotron";
 
 function App() {
   return (
     <Router>
       <div>
         <Nav />
+        <Jumbotron />
         <Switch>
           <Route exact path="/">
             <Search />
---
+++ client/src/components/Jumbotron/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Jumbotron() {
+    return (
+        <div class="jumbotron jumbotron-fluid">
+            <div class="container">
+                <h1 class="display-4 text-center">Google Books Search</h1>
+                <p class="lead text-center">Search for and Save Books of Interest</p>
+            </div>
+        </div>
+    );
+}
+
+export default Jumbotron;
