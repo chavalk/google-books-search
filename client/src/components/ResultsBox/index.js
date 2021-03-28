78b2c96b694d8d569fe709a52efd728b3947bcad
chavalk <chavalk@hotmail.com>
2021-03-28 16:25:03 -0500

    Create results box component with initial code and add component to search page.

---
+++ client/src/components/ResultsBox/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function ResultsBox() {
+    return (
+        <div class="jumbotron jumbotron-fluid">
+            <div class="container">
+                <p class="lead">Results</p>
+                <div class="jumbotron jumbotron-fluid">
+                    <div class="container">
+                        <p class="lead">Harry Potter</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ResultsBox;
--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,10 +1,12 @@
 import React from "react";
 import SearchBox from "../components/SearchBox";
+import ResultsBox from "../components/ResultsBox";
 
 function Search() {
     return (
         <div>
             <SearchBox />
+            <ResultsBox />
         </div>
     );
 }
