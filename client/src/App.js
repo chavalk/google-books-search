e1e7ed955eeb4e74b17b1ce98ca0926a71e4dd8d
chavalk <chavalk@hotmail.com>
2021-03-28 14:47:01 -0500

    Add nav component in app.js, add components folder with nav folder and index.js with code for nav, and import bootstrap to index.js.

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,15 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Search from "./pages/Search";
 import Saved from "./pages/Saved";
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <Router>
       <div>
+        <Nav />
         <Switch>
-          <Route exact path="/search">
+          <Route exact path="/">
             <Search />
           </Route>
           <Route exact path="/saved">
