2e79101a686ca10b644e7e74422d6edac18c84f4
chavalk <chavalk@hotmail.com>
2021-03-27 23:00:38 -0500

    Add saved page with initial code and add route to saved page in app.js.

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Search from "./pages/Search";
+import Saved from "./pages/Saved";
 
 function App() {
   return (
@@ -9,6 +10,9 @@ function App() {
         <Switch>
           <Route exact path="/search">
             <Search />
+          </Route>
+          <Route exact path="/saved">
+            <Saved />
           </Route>
         </Switch>
       </div>
