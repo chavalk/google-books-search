41de2bf4365f59674c6ccbccdd45e51a12d06243
chavalk <chavalk@hotmail.com>
2021-03-29 18:06:21 -0500

    Modify view button to act as a link to view more info on each of the books.

--- client/src/components/ResultsList/index.js
+++ client/src/components/ResultsList/index.js
@@ -8,7 +8,7 @@ function ResultsList(props) {
                 <li className="list-group-item" key={result.id}>
                     <p className="lead d-inline">{result.volumeInfo.title}</p>
                     <button className="btn btn-success" style={{ float: "right" }}>Save</button>
-                    <button className="btn btn-success mr-1" style={{ float: "right" }}>View</button>
+                    <a className="btn btn-success mr-1" style={{ float: "right" }} href={result.volumeInfo.infoLink} target="_blank" rel="noreferrer">View</a>
                     <p className="lead">Written by {result.volumeInfo.authors}</p>
                     <img src={result.volumeInfo.imageLinks.smallThumbnail} className="img-thumbnail float-left mr-3" alt="Book"></img>
                     <p className="lead">{result.volumeInfo.description}</p>
